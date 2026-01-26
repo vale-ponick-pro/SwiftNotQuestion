816ca7e6581e04adf75ba14580f08dc06e5fa946
Valery Ponomareva <vale.ponick@gmail.com>
2026-01-27 02:48:56 +0700

    "feat: solved tasks"

--- Swift?NotQuestion/Basics/basics.swift
+++ Swift?NotQuestion/Basics/basics.swift
@@ -6,5 +6,88 @@
 //
 
 import Foundation
+// --- ГЛАВНАЯ ФУНКЦИЯ ---
+func runProgram() {
+    task1()
+    task2()
+    task3()
+    task4()
+    task5()
+    task6()
+    task7()
+}
 
-/* "Task 1: выведи имя юзера и выведи сообщение: 'Hi, [name]!'") */
+func task1() {
+    print("Task 1: Введи свое имя:")
+    guard let name = readLine(), !name.isEmpty else {
+        print("Имя не введено")
+        return
+    }
+    print("Hi, \(name)") // Hi, vale!
+}
+
+func task2() {
+    print("Task 2: Введи свое имя:")
+    guard let name = readLine()?.trimmingCharacters(in: .whitespaces) else { return }
+    print("Введи фамилию:")
+    guard let surname = readLine()?.trimmingCharacters(in: .whitespaces) else { return }
+    
+    print("Hello, \(name) \(surname)")
+}
+
+func task3() {
+    print("Task 3: введи стихи построчно:")
+    print("""
+    Fifteen men on the dead man'schest -
+    Yo-ho-h0 and the bottle of rum!
+    Drink and the devil had done for the rest -
+    Yo-ho-h0 and the bottle of rum!
+    """)
+}
+
+func safeDoubleInput(prompt: String) -> Double? {
+    print(prompt)
+    guard let input = readLine(),
+          let value = Double(input.trimmingCharacters(in: .whitespaces)) else {
+        print("Incorrect number format")
+        return nil
+    }
+    return value
+}
+
+func task4() {
+    print("Task 4:")
+    guard let a = safeDoubleInput(prompt: "Введи число № 1:"),
+          let b = safeDoubleInput(prompt: "Введи число № 2:") else { return }
+    
+    print("Сумма = \(a + b)") // Сумма = 33
+}
+
+func task5() {
+    print("Task 5:")
+    guard let x = safeDoubleInput(prompt: "Введи первое дробное число:"),
+          let y = safeDoubleInput(prompt: "Введи второе дробное число:"),
+          let z = safeDoubleInput(prompt: "Введи третье дробное число:"), z != 0 else {
+              print("Error! Division on 0 or incorrect data")
+              return
+          }
+    
+    let result = (x + y) / z
+    print("Result = \(result)")
+}
+
+func task6() {
+    print("Task 6: сколько друзей было в баре? Сумма счета? посчитай - сколько выходит с одного и выведи сообщение 'Значит с каждого по [столько то] рублей'")
+    guard let total = safeDoubleInput(prompt: "Введи сумму общего счета (руб.):"),
+          let friendsCount = safeDoubleInput(prompt: "Введи количество друзей:"), friendsCount > 0 else {
+        print("❌ Ошибка ввода!")
+        return
+    }
+   
+    let perPerson = total / friendsCount
+    let roundedAmount = String(format: "%.2f", perPerson)
+    print("💰 Значит с каждого по \(perPerson) рублей")
+}
+
+func task7() {
+}
--- Swift?NotQuestion/main.swift
+++ Swift?NotQuestion/main.swift
@@ -6,32 +6,6 @@
 //
 import Foundation
 
-print("Task 1: предолжи юзеру ввести его имя и выведи сообщение: 'Hi, name!'")
-
-print("Введи свое имя:")
-let name = readLine()
-
-print("Hi, \(name!)!") // Hi, vale!
-
-print("Task 2: предолжи юзеру ввести его имя и фамилию и выведи сообщение: 'Hi, [name] [surname]!'")
-
-print("Введи свое имя и фамилию:")
-let surname = readLine()
-
-print("Hello, \(name!) \(surname!)!")
-
-print("Task 3: выведи стихи построчно: Fifteen men on the dead man'schest - Yo-ho-h0 and the bottle of rum! Drink and the devil had done for the rest - Yo-ho-h0 and the bottle of rum!")
-
-print("Fifteen men on the dead man'schest -\nYo-ho-h0 and the bottle of rum!\nDrink and the devil had done for the rest -\nYo-ho-h0 and the bottle of rum!")
-/* Fifteen men on the dead man'schest -
-Yo-ho-h0 and the bottle of rum!
-Drink and the devil had done for the rest -
-Yo-ho-h0 and the bottle of rum! */
-
-print("Task 4: предложи юзеру ввести два числа. Сложи эти числа и выведи результат на консоль")
-print("Введи два числа:")
-let numberOne = readLine()
-let numberTwo = readLine()
-
+runProgram()
 
 
